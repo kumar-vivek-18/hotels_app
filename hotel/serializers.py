637af392f568934e38c5461fa06695acc27570dd
Vivek <vpwr2004@gmail.com>
2025-03-19 07:22:08 +0530

    hotel create api

--- hotel/serializers.py
+++ hotel/serializers.py
@@ -12,9 +12,7 @@ class HotelImagesSerializer(serializers.ModelSerializer):
         model = HotelImages
         fields = ['id', 'image_url']
 
-class HotelSerializer(serializers.ModelSerializer):
-    hotel_amenities = HotelAmenitiesSerializer(many = True,required=False, read_only=True)
-    hotel_images = HotelImagesSerializer(many = True, required=False, read_only=True)
+class HotelInputSerializer(serializers.ModelSerializer):
     hotel_amenities = serializers.ListField(child=serializers.CharField(), write_only = True,required=False)
     hotel_images = serializers.ListField(child=serializers.ImageField(), write_only = True, required=False)
 
@@ -27,13 +25,10 @@ class HotelSerializer(serializers.ModelSerializer):
         ]
 
     def create(self, validated_data):
-        print("*****", validated_data)
         amenities_data = validated_data.pop('hotel_amenities',[])
         images_data = validated_data.pop('hotel_images',[])
 
         hotel = Hotel.objects.create(**validated_data)
-        print("********* amenities", amenities_data)
-        print("********** images", images_data)
         for amenity_data in amenities_data:
             if len(amenity_data)>0:
                 amenity, _ = HotelAmenities.objects.get_or_create(name=amenity_data)
@@ -43,4 +38,18 @@ class HotelSerializer(serializers.ModelSerializer):
             if image_data:
                 image = HotelImages.objects.create(image_url=image_data)
                 hotel.hotel_images.add(image)
+                
         return hotel
+
+
+class HotelOutputSerializer(serializers.ModelSerializer):
+    hotel_amenities = HotelAmenitiesSerializer(many=True)
+    hotel_images = HotelImagesSerializer(many=True)
+
+    class Meta:
+        model = Hotel
+        fields = [
+            'id', 'hotel_name','hotel_description','hotel_email','hotel_phone', 'hotel_owner', 'hotel_category', 'hotel_address',
+            'hotel_city', 'hotel_state', 'hotel_longitude', 'hotel_latitude',
+            'hotel_amenities', 'hotel_images', 'is_active', 'created_at', 'updated_at'
+        ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 
-from rest_framework import generics
-from .serializers import HotelSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import HotelInputSerializer, HotelOutputSerializer
 from .models import Hotel
 
 
 class HotelCreateView(generics.CreateAPIView):
     queryset = Hotel.objects.all()
-    serializer_class = HotelSerializer
+    serializer_class = HotelInputSerializer
 
-    def post(self, request, *args, **kwargs):
-        print("********* req data", request.data)
-        return super().post(request, *args, **kwargs)
+    def create(self, request, *args, **kwargs):
+        input_serializer = self.get_serializer(data = request.data)
+        input_serializer.is_valid(raise_exception = True)
+        hotel = input_serializer.save()
+        
+        output_serailizer = HotelOutputSerializer(hotel)
+        headers = self.get_success_headers(output_serailizer.data)
+        return Response(output_serailizer.data, status=status.HTTP_201_CREATED, headers=headers)
